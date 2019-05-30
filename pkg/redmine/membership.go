b914e63a09d3a3f12ea89af9c2c4722483dec29b
びくんびくん <big.stone.net@gmail.com>
2019-05-31 08:47:21 +0900

    mod membership

--- pkg/redmine/membership.go
+++ pkg/redmine/membership.go
@@ -1,4 +1,19 @@
 package redmine
 
-type membership struct {
+type membershipsResult struct {
+	Memberships []Membership `json:"memberships"`
+	TotalCount  int          `json:"total_count"`
+	Limit       int          `json:"limit"`
+	Offset      int          `json:"offset"`
+}
+
+type Membership struct {
+	ID      int       `json:"id"`
+	Project IdName    `json:"project"`
+	User    IdName    `json:"user"`
+	Roles   []*IdName `json:"roles"`
+}
+
+func (c *Client) getMemberships(project string) ([]*Membership, error) {
+	return nil, nil
 }
