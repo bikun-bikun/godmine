e4966ad15124aacaad012cc1525642c9cf1fc842
びくんびくん <big.stone.net@gmail.com>
2019-06-03 14:50:14 +0900

    add config create method

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"errors"
 	"github.com/go-yaml/yaml"
 	"io/ioutil"
+	"os"
 )
 
 const (
@@ -28,6 +30,26 @@ func Load(profile string) (*Config, error) {
 	return &cnf, nil
 }
 
+func NewConfig(profile string, ep string, k string) error {
+	path := getFilePath(profile)
+	if !Exists(path) {
+		return errors.New("File Exists")
+	}
+	c := &Config{ep, k}
+	y, err := yaml.Marshal(c)
+	if err != nil {
+		return errors.New("yaml Marshal error")
+	}
+	if err = ioutil.WriteFile(path, y, 0644); err != nil {
+		return errors.New("Create Config File error")
+	}
+	return nil
+}
+
+func Exists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
 func getFilePath(profile string) (path string) {
 	path = basePath + profile + extension
 	return
